216df49425b52862af0c959acbec92954b087f80
yitzchake <yitzchake@reblaze.com>
2023-04-01 18:44:32 +0300

    Different modes, different speeds

--- MobileGame/scenes/MainGame/MainGame.gd
+++ MobileGame/scenes/MainGame/MainGame.gd
@@ -5,28 +5,19 @@ var text_timer = Timer.new()
 var transition_period = 2
 var transition_position = 0
 
-const MEGAMAN = preload("res://characters/megaman/megaman.tscn")
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_child(text_timer)
-	text_timer.one_shot = true
-	text_timer.timeout.connect(_on_text_timer_timeout)
-#	text_timer.connect("timeout", self, "_on_text_timer_timeout")
+	text_timer.connect("timeout", self, "_on_text_timer_timeout")
 	text_timer.start(2)
 	print("Mode: ", PersistentData.mode)
-	$TitleScreenTouchScreenButton.pressed.connect(_return_to_title_screen)
-	#$TitleScreenTouchScreenButton.connect("pressed", self, "_return_to_title_screen")
-
+	$TitleScreenTouchScreenButton.connect("pressed", self, "_return_to_title_screen")
+	
 
 func _return_to_title_screen():
-	get_tree().change_scene_to_file("res://scenes/TitleScreen/TitleScreen.tscn")
+	SceneLoader.goto_scene("res://scenes/TitleScreen/TitleScreen.tscn")
 
 
 func _on_text_timer_timeout():
 	var print_mode = "Playing on {mode} mode".format({"mode": PersistentData.mode})
-	print(print_mode)
-	$RichTextLabel.text = print_mode
-	var megaman = MEGAMAN.instantiate()
-	add_child(megaman)
-	megaman.position = Vector2(400,400)
+	$Tween/RichTextLabel.text = print_mode
--- MobileGame/scenes/TitleScreen/TitleScreen.gd
+++ MobileGame/scenes/TitleScreen/TitleScreen.gd
@@ -9,28 +9,19 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("starting")
-	$EasyModeTouchScreenButton.pressed.connect(_on_easy_mode_pressed)
-	$MediumModeTouchScreenButton.pressed.connect(_on_medium_mode_pressed)
-	$HardModeTouchScreenButton.pressed.connect(_on_hard_mode_pressed)
-	#$EasyModeTouchScreenButton.is_pressed(self._on_easy_mode_pressed())
-	#$MediumModeTouchScreenButton.is_pressed(self._on_medium_mode_pressed())
-	#$HardModeTouchScreenButton.is_pressed(self._on_hard_mode_pressed())
-
-#func _process(delta):
-#	if ($HardModeTouchScreenButton.is_pressed()):
-#		print("hard mode pressed")
+	$EasyModeTouchScreenButton.connect("pressed", self, "_on_easy_mode_pressed")
+	$MediumModeTouchScreenButton.connect("pressed", self, "_on_medium_mode_pressed")
+	$HardModeTouchScreenButton.connect("pressed", self, "_on_hard_mode_pressed")
+	
 
 func _on_easy_mode_pressed():
-	print("easy pressed")
-	get_tree().change_scene_to_file("res://scenes/MainGame/MainGame.tscn")
+	SceneLoader.goto_scene("res://scenes/MainGame/MainGame.tscn")
 	PersistentData.mode = "easy"
 
 func _on_medium_mode_pressed():
-	print("medium pressed")
-	get_tree().change_scene_to_file("res://scenes/MainGame/MainGame.tscn")
+	SceneLoader.goto_scene("res://scenes/MainGame/MainGame.tscn")
 	PersistentData.mode = "medium"
 
 func _on_hard_mode_pressed():
-	print("hard pressed")
-	get_tree().change_scene_to_file("res://scenes/MainGame/MainGame.tscn")
+	SceneLoader.goto_scene("res://scenes/MainGame/MainGame.tscn")
 	PersistentData.mode = "hard"
